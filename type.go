453950b7cfdc452fd7ab04421dcce6c2abf49c3c
程勇桥 <sion@chengyongqiaos-MacBook-Air.local>
2014-03-11 00:24:56 +0800

    type practice

---
+++ type.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	FirstName string;
+	LastName string
+}
+
+func(e *Person) Hello() string{
+	return "My name is " + e.FirstName + " " + e.LastName;
+}
+
+func main() {
+	var p Person;
+
+	p.FirstName = "Sion";
+	p.LastName = "Cheng";
+
+	fmt.Println(p.Hello());
+}
